a8e6d247524284e6e5a8e9e16c230980ecbe9b2f
JohnRowleyDorsetCollege <john.rowley@dorset.ie>
2025-09-30 11:12:46 +0100

    Add project files.

---
+++ oop-2025-09-30-b-switch-with-exceptions/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Switch Expressions");
+
+// generate a random number between 1 and 10
+Random random = new Random();
+int number = random.Next(1, 5);
+
+Console.WriteLine($"Generated number: {number}");
+
+//output the number in words using a switch statement
+switch (number)
+{
+    case 1:
+        Console.WriteLine("One");
+        break;
+    case 2:
+        Console.WriteLine("Two");
+        break;
+    case 3:
+        Console.WriteLine("Three");
+        break;
+    case 4:
+        Console.WriteLine("Four");
+        break;
+    default:
+        throw new Exception("Number is out of range");
+}
+
+string result = number switch
+{
+    1 => "One",
+    2 => "Two",
+    3 => "Three",
+    _ => "Number not recognized"
+};
+
+Console.WriteLine($"Switch Expression: {result}");
+
+//else
+//{
+//    
+//}
