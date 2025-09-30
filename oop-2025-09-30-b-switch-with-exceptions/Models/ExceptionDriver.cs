1a73fa62bc0920a3fc5cd18d3b57ba82b528648e
JohnRowleyDorsetCollege <john.rowley@dorset.ie>
2025-09-30 12:05:54 +0100

    Added IO Exception

---
+++ oop-2025-09-30-b-switch-with-exceptions/Models/ExceptionDriver.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_2025_09_30_b_switch_with_exceptions.Models
+{
+    public static class ExceptionDriver
+    {
+        public static void Run()
+        {
+            Console.WriteLine("Exception Demos");
+            IOExceptionDemo();
+        }
+        public static void IOExceptionDemo()
+        {
+
+            string filePath = "this-file-does-not-exist.txt";
+            try
+            {
+                string content = File.ReadAllText(filePath);
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine($"IOException\t caught:\t [ {ex.Message} ]");
+            }
+        }
+
+    }
+}
--- oop-2025-09-30-b-switch-with-exceptions/Program.cs
+++ oop-2025-09-30-b-switch-with-exceptions/Program.cs
@@ -1,57 +1,82 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Switch Expressions");
 
-// generate a random number between 1 and 10
-Random random = new Random();
-int number = random.Next(1, 5);
+using oop_2025_09_30_b_switch_with_exceptions.Models;
 
-Console.WriteLine($"Generated number: {number}");
+ExceptionDriver.Run();
+System.Environment.Exit(0);
 
-//output the number in words using a switch statement
-switch (number)
+
+//Cnsole.WriteLine("Switch Expressions");
+
+//// generate a random number between 1 and 10
+//Random random = new Random();
+//int number = random.Next(1, 5);
+
+//Console.WriteLine($"Generated number: {number}");
+
+////output the number in words using a switch statement
+//switch (number)
+//{
+//    case 1:
+//        Console.WriteLine("One");
+//        break;
+//    case 2:
+//        Console.WriteLine("Two");
+//        break;
+//    case 3:
+//        Console.WriteLine("Three");
+//        break;
+//    case 4:
+//        Console.WriteLine("Four");
+//        break;
+//    default:
+//        throw new Exception("Number is out of range");
+//}
+
+//string result = number switch
+//{
+//    1 => "One",
+//    2 => "Two",
+//    3 => "Three",
+//    _ => "Number not recognized"
+//};
+
+//Console.WriteLine($"Switch Expression: {result}");
+
+//Console.Write("Enter a color: ");
+//string colour = Console.ReadLine().ToLower(); // Normalize input
+
+//switch (colour)
+//{
+//    case "red":
+//    case "orange":
+//    case "yellow":
+//    case "green":
+//    case "blue":
+//    case "indigo":
+//    case "violet":
+//        Console.WriteLine($"{colour} is a color in the rainbow.");
+//        break;
+
+//    default:
+//        Console.WriteLine($"{colour} is NOT a color in the rainbow.");
+//        break;
+//}
+
+string filePath = "this-file-does-not-exist.txt";
+
+try
 {
-    case 1:
-        Console.WriteLine("One");
-        break;
-    case 2:
-        Console.WriteLine("Two");
-        break;
-    case 3:
-        Console.WriteLine("Three");
-        break;
-    case 4:
-        Console.WriteLine("Four");
-        break;
-    default:
-        throw new Exception("Number is out of range");
+string content = File.ReadAllText(filePath);
 }
 
-string result = number switch
+catch (Exception ex) when (ex is FileNotFoundException || ex is DirectoryNotFoundException)
 {
-    1 => "One",
-    2 => "Two",
-    3 => "Three",
-    _ => "Number not recognized"
-};
-
-Console.WriteLine($"Switch Expression: {result}");
-
-Console.Write("Enter a color: ");
-string colour = Console.ReadLine().ToLower(); // Normalize input
-
-switch (colour)
-{
-    case "red":
-    case "orange":
-    case "yellow":
-    case "green":
-    case "blue":
-    case "indigo":
-    case "violet":
-        Console.WriteLine($"{colour} is a color in the rainbow.");
-        break;
-
-    default:
-        Console.WriteLine($"{colour} is NOT a color in the rainbow.");
-        break;
+    Console.WriteLine("File or directory not found.");
 }
+
+catch (Exception ex)
+{
+    Console.WriteLine($"An unexpected error occurred: {ex.Message}");
+}
+
